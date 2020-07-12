6c7f540da4e36bcaa31d1401f01afc163b4e6020
eacz <zuzu620@hotmail.com>
2020-07-12 19:33:01 -0300

    settings for deployment

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const conectDB = require('./config/db');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 conectDB();
-app.use(cors())
+app.use(cors());
 app.use(express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //routes
 app.use('/api/users', require('./routes/users'));
@@ -15,6 +15,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Workinggg on port ${PORT}`);
 });
